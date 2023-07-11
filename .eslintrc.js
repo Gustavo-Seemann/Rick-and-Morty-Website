7804bbff74a7a5d362c50ac2b56a0729a97d5c8c
Luiz Gustavo S <seemann24@hotmail.com>
2023-07-11 20:09:54 -0300

    chore: edit linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,17 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-strongly-recommended", "eslint:recommended"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
+  plugins: ["vue"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: ["error", "always"],
+    indent: ["error", 2],
+    "comma-spacing": ["error", { before: false, after: true }],
+    "vue/no-multi-spaces": 0,
   },
 };
